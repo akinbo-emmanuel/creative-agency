23fdc7056097a72ff4df21cea2d45ce16c8a6b42
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-09-06 10:52:26 +0100

    Nav(mobile) - UPDATED

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,18 +4,12 @@ import hamburger from '../assets/ham.svg'
 
 const Nav = () => {
   return (
-    <header className='w-[21.25rem] mt-[2.5rem] mx-[1.56rem] h-[2.06rem]'>
-      <nav className='inline-flex content-center justify-center flex-row gap-[13.75rem] h-full'>
-        <div className="logo">
-          <a href="#home">
-            <img src={logo} alt="Our logo" className='w-[5.68rem] h-[2.06rem]'/>
-          </a>
-        </div>
-        <div className="content-center">
-          <button className='h-full'>
-            <img src={hamburger} alt="Hamburger menu" className='w-[1.8rem] h-[1.56rem]'/>
-          </button>
-        </div>
+    <header className='flex justify-center mt-[2.5rem] '>
+      <nav className=' w-[21.25rem] h-[2.06rem] inline-flex items-center justify-center gap-[13.75rem] '>
+        <img src={logo} alt="Our logo" className='w-[5.68rem] h-[2.06rem]'/>
+        <button className='h-full'>
+          <img src={hamburger} alt="Hamburger menu" className='w-[1.8rem] h-[1.56rem]'/>
+        </button>
       </nav>
     </header>
   )
